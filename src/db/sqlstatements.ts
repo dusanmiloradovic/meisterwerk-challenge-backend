f2f14892408955c0903b971b5f0fa77fee530ece
Dusan Miloradovic <dusan.m@maximoplus.com>
2022-07-14 07:18:11 +0400

    typo

--- src/db/sqlstatements.ts
+++ src/db/sqlstatements.ts
@@ -1,7 +1,7 @@
 //export const GET_ALL_EVENTS =
 //    'select events_id,title,start_time,end_time,status,a.address from events e, addresses a where a.address_id=e.address_id and start_time between 1656619200000 and 1659211200000  limit 100'; //limit records for the demo only July
 export const GET_ALL_EVENTS =
-  'select events_id,title,start_time,end_time,status,a.address from events e, addresses a where a.address_id=e.address_id'; //limit records for the demo only July
+  'select events_id,title,start_time,end_time,status,a.address from events e, addresses a where a.address_id=e.address_id';
 export const INSERT_EVENT = 'insert into events(title,start_time,end_time,status,address_id) values(?,?,?,?,?)';
 export const GET_ADDRESS_ID = 'select address_id from addresses where address=?';
 export const INSERT_ADDRESS = 'insert into addresses(address) values(?)';
